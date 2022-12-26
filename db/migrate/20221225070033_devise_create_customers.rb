36c1d7adb91219648dfa983f0edae6f45264b591
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-12-26 13:37:12 +0000

    [update]カラム追加/activestrage追加

--- db/migrate/20221225070033_devise_create_customers.rb
+++ db/migrate/20221225070033_devise_create_customers.rb
@@ -31,7 +31,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      t.string :last_name, null: false, default: ""
+      t.string :first_name, null: false, default: ""
+      t.string :last_name_kana, null: false, default: ""
+      t.string :first_name_kana, null: false, default: ""
+      t.string :address, null: false, default: ""
+      t.string :post_code, null: false, default: ""
+      t.string :phone_number, null: false, default: ""
+      t.boolean :is_deleted, null: false, default: false
 
       t.timestamps null: false
     end
